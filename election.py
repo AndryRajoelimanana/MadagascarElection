22d2732d64de06719e8a1f64b2d7434ff515aa42
Andry Rajoelimanana <randryfitiavana@gmail.com>
2018-05-22 01:51:06 -0400

    upload to pythonanywhere

--- election.py
+++ election.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+    
+@app.route('/hvm.html')
+def main():
+    return render_template('hvm.html')    
+    
 
 if __name__ == '__main__':
     app.run(debug=True, threaded = True)
